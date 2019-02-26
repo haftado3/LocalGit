123290f962c90a669dc237f69161e146b62e539c
Mahdi Khalili <Mahdi Khalili@DESKTOP-9CF4PCU>
2019-02-26 19:36:41 +0330

    Adaptive Menu

---
+++ LocalGit/Converters/IsSmallResolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LocalGit.Converters
+{
+    class IsSmallResolution : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var intParam = int.Parse(parameter.ToString());
+            if(intParam!= 0)
+            {
+                if ((double)value < intParam)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocalGit/Entities/Menu/MenuItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace LocalGit.Entities.Menu
+{
+    public class MenuItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private List<MenuItem> _children;
+        List<MenuItem> Children
+        {
+            get { return _children; }
+            set
+            {
+                _children = value;
+                NotifyPropertyChanged(nameof(Children));
+            }
+        }
+        private string _header;
+        public string Header
+        {
+            get
+            {
+                return _header;
+
+            }
+            set
+            {
+                _header = value;
+                NotifyPropertyChanged(nameof(Header));
+            }
+        }
+        private BitmapSource _icon;
+        public BitmapSource Icon
+        {
+            get
+            {
+                return _icon;
+            }
+            set
+            {
+                _icon = value;
+                NotifyPropertyChanged(nameof(Icon));
+            }
+        }
+        private string _action;
+        public string Action
+        {
+            get
+            {
+                return _action;
+            }
+            set
+            {
+                _action = value;
+                NotifyPropertyChanged(nameof(Action));
+            }
+        }
+    }
+}
